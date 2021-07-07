2428aa9ab3bb61bf28f7ac98ee932460cbc96435
Anna3024 <kurban.anna@ab-ds.ru>
2021-07-07 09:08:53 +0300

    init backend for project tickets

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const {Schema, model, Types} = require ('mongoose')
+
+const schema = new Schema({
+    email:{
+        type: String,
+        require: true, //обязательное поле
+        unique: true //уникальный e-mail
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    gender: {
+        type: Boolean,
+    },
+    phone: {
+        type: Number,
+        unique: true
+    },
+    birthday: {
+        type: String
+    },
+    orders: [{                       //заказанные билеты
+        type: Types.ObjectId,
+        ref: 'Order'
+    }]
+
+
+})
+
+module.exports = model('User', schema)
